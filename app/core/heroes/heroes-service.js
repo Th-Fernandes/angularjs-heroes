765aa6859092c00ddeb156e59d31ea5bccde01a3
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-01-02 11:25:10 -0300

    feat: last hero state

--- app/core/heroes/heroes-service.js
+++ app/core/heroes/heroes-service.js
@@ -3,19 +3,46 @@ angular
   .factory('HeroesService', [
     '$resource',
     class HeroesService {
-      heroesEndPoint = 'https://randomuser.me/api/?results=5';
+      HEROES_ENDPOINT = 'https://randomuser.me/api/?results=5';
       storedValue = [];
+      lastHero = {}
   
       constructor($resource) {
-        console.log(uuidv4())
-        $resource(this.heroesEndPoint).get().$promise
-          .then(heroes => this.storedValue.push(...heroes.results));
+        this.$resource = $resource;
+
+        $resource(this.HEROES_ENDPOINT).get().$promise
+          .then(heroes => {
+            this.storedValue.push(...heroes.results);
+            const lastHero = heroes.results.at(-1);
+            Object.assign(this.lastHero, lastHero);
+          });
+
+        this.storedHeroes = $resource(this.HEROES_ENDPOINT,
+          null,
+          {
+            get: {
+              method: 'GET',
+              isArray: true,
+              transformResponse: res => {
+                res = JSON.parse(res);
+                return res.results;
+              }
+            },
+            getLast: {
+              method: 'GET',
+              transformResponse: (res) => {
+                res = JSON.parse(res);
+                return res.results.at(-1);
+              }
+            }
+          })
       }
-  
+
       addNewHero(hero) {
         this.storedValue.push({...hero, login: { uuid: uuidv4() }});
+        Object.assign(this.lastHero, hero);
       }
-  
+
       getLoginUuid(uuid) {
         return this.storedValue.find(hero => hero.login.uuid === uuid)
       }

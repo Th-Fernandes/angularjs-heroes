e5a8994bb8a99a8b551219c91d30d4bb2a077bb1
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-01-09 09:49:03 -0300

    feat: redirect signed in user

--- app/features/sign-in/sign-in.config.js
+++ app/features/sign-in/sign-in.config.js
@@ -1,13 +1,20 @@
-angular
-  .module("myApp.signIn", ['myApp.signIn.signInFormComponent'])
+angular.module("myApp.signIn", ["myApp.signIn.signInFormComponent"])
   .config([
     "$routeProvider",
     class {
       constructor($routeProvider) {
-        $routeProvider
-          .when("/sign-in", {
-            template: '<sign-in-form></sign-in-form>'
-          })
+        $routeProvider.when("/sign-in", {
+          template: "<sign-in-form></sign-in-form>",
+          controller: RedirectSignedInUserController,
+        });
       }
-    }
-  ])
+    },
+  ]);
+
+const RedirectSignedInUserController = [
+  "$location", "JwtService",
+  function ($location, JwtService) {
+    const { isTokenUnavailable } = JwtService.getToken();
+    if (!isTokenUnavailable) $location.path("/heroes");
+  },
+];

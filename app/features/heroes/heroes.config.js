c70efffcf239ad193c0651fb2501d9ed174de2fd
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-01-10 10:52:52 -0300

    refactor: change component name

--- app/features/heroes/heroes.config.js
+++ app/features/heroes/heroes.config.js
@@ -6,7 +6,7 @@ angular
     "myApp.heroes.listComponent", 
     "myApp.heroes.addHeroFormComponent",
     "myApp.heroes.heroDetailsComponent",
-    "myApp.heroes.heroOpportunityComponent",
+    "myApp.heroes.heroOpportunitiesComponent",
   ])
   .config([
     "$routeProvider",

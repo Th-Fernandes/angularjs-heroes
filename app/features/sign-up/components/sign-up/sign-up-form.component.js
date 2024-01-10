fdefe9b139d744424f7f55502c21426d076b8c0e
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-01-10 15:24:24 -0300

    feat: sign up inputs

--- app/features/sign-up/components/sign-up/sign-up-form.component.js
+++ app/features/sign-up/components/sign-up/sign-up-form.component.js
@@ -1,5 +1,37 @@
 angular
   .module('myApp.signUp.signUpFormComponent', [])
   .component('signUpForm', {
-    templateUrl: 'features/sign-up/components/sign-up/sign-up-form.html'
-  })
+    templateUrl: 'features/sign-up/components/sign-up/sign-up-form.html',
+    controller: [
+      function() {
+        this.inputs = new SignUpInputGroupFactory();
+
+        this.onSubmitSignUp = () => {
+          console.log(this.inputs)
+        }
+      }
+    ]
+  });
+
+
+  class SignUpInputGroupFactory {
+    name = {first: '', name: ''}
+    email = {
+      value: '',
+      pattern: ''
+    }
+    photo = {
+      value: '',
+      pattern: '/a/'
+    }
+    gender = {
+      value: ''
+    }
+    codename = {
+      value: ''
+    }
+    phoneNumber = {
+      value: '',
+      pattern: '/a/',
+    }
+  }

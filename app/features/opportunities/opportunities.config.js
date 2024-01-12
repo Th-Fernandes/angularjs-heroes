1c157658aaafebcec7dc895147453d47c06ddf85
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-01-12 10:51:28 -0300

    feat: turn millisecond into date

--- app/features/opportunities/opportunities.config.js
+++ app/features/opportunities/opportunities.config.js
@@ -4,6 +4,7 @@ angular
   .module("myApp.opportunities", [
     'myApp.opportunities.opportunitiesService',
     'myApp.opportunities.opportunitiesListComponent',
+    'myApp.opportunities.millisecondsToDate'
   ])
   .config([
     "$routeProvider",

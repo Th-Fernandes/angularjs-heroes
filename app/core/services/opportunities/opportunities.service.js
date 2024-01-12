44e18ef943d6011a56cdded5b689eb17d8718e71
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-01-12 15:05:18 -0300

    feat: fetch opportunities from signed in user

--- app/core/services/opportunities/opportunities.service.js
+++ app/core/services/opportunities/opportunities.service.js
@@ -1,26 +1,45 @@
 angular
-  .module('myApp.opportunitiesService', [])
-  .factory('OpportunitiesService', [
-    '$resource', 'JwtService',
+  .module("myApp.opportunitiesService", [])
+  .factory("OpportunitiesService", [
+    "$resource",
+    "$q",
+    "JwtService",
     class {
       #opportunities;
+      #SIGNED_IN_USER_ID;
 
-      constructor($resource, JwtService) {
-        console.log(JwtService.getToken())
-        this.#opportunities = $resource('http://localhost:3000/opportunities?_sort=createdAt&_order=desc')
-          .query()
+      constructor($resource, $q, JwtService) {
+        this.$q = $q;
+        this.#SIGNED_IN_USER_ID = JwtService.getToken().value;
+        this.#opportunities = $resource(
+          "http://localhost:3000/opportunities?_sort=createdAt&_order=desc"
+        ).query();
       }
 
       get() {
+        return this.#opportunities.$promise;
+      }
+
+      getByCreatorId() {
         return this.#opportunities.$promise
+          .then((opportunities) => {
+            const signedInUserOpportunities = this.#filterByCreatorId(opportunities)
+            return this.$q.resolve(signedInUserOpportunities);
+          })
+          .catch(() => $q.reject('API is not responding'));
+
+      }
+
+      #filterByCreatorId(opportunities) {
+        return opportunities.filter(o => o.creatorID === this.#SIGNED_IN_USER_ID)
       }
 
       opportunitiesPromiseFactory() {
-        return  {
-          data: undefined , 
-          hasFetchFailed: false, 
-          isFetchLoading: true
-        }
+        return {
+          data: undefined,
+          hasFetchFailed: false,
+          isFetchLoading: true,
+        };
       }
-    }
-  ])
+    },
+  ]);

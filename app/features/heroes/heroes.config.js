b9359b9b17ec719bfb58a61eebdaf6b3a0497105
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-01-16 17:46:29 -0300

    feat: use notify method on heroes list

--- app/features/heroes/heroes.config.js
+++ app/features/heroes/heroes.config.js
@@ -13,8 +13,10 @@ angular
       $routeProvider
         .when("/heroes", {
           template: 
-            '<heroes-list> </heroes-list>'+
-            '<add-hero-form> </add-hero-form>',      
+            '<page-errors-handler>'+
+              '<heroes-list> </heroes-list>'+
+              '<add-hero-form> </add-hero-form>'+     
+            '</page-errors-handler>',
         })
         .when("/heroes/:uuid", {
           template: '<hero-details> </hero-details>',

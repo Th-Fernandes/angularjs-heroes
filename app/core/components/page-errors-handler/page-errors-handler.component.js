a8888518a619293dd2ff74481ad2d50de215ec0a
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-01-16 16:24:23 -0300

    feat: conditional rendering

--- app/core/components/page-errors-handler/page-errors-handler.component.js
+++ app/core/components/page-errors-handler/page-errors-handler.component.js
@@ -2,7 +2,10 @@ angular
   .module('myApp.core.components.pageErrorsHandlerComponent', [])
   .component('pageErrorsHandler', {
     templateUrl: 'core/components/page-errors-handler/page-errors-handler.html', 
+    transclude: true,
     controller: [
-      function() { }
+      function() { 
+        this.errorHandler = {hasErrors: true}
+      }
     ],
   })

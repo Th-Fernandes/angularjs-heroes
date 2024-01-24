ebad0d00a47afcd86598fc1bec9a2a0c29d8e6eb
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-01-24 14:41:14 -0300

    refactor Hero model on different module

--- app/core/services/auth/auth.module.js
+++ app/core/services/auth/auth.module.js
@@ -2,4 +2,4 @@ import { AuthService } from "./auth.service.js";
 
 angular
   .module("myApp.auth", ["myApp.auth.jwtService"])
-    .factory("AuthService", AuthService);
+  .factory("AuthService", AuthService);

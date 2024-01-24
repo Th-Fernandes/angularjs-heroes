db80c13e2b0aedc3d5c896754cb986253bcb97f3
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-01-24 10:55:59 -0300

    feat: toggle button depending on apply status

--- app/features/opportunities/components/opportunities-list/opportunities-list.component.js
+++ app/features/opportunities/components/opportunities-list/opportunities-list.component.js
@@ -5,20 +5,24 @@ angular
     controller: [
       'OpportunitiesService', 'PageErrorsHandlerService', 'JwtService',
       function(OpportunitiesService, PageErrorsHandlerService, JwtService) {
-          this.opportunities = OpportunitiesService.GETLifeCycle();
+        this.opportunities = OpportunitiesService.GETLifeCycle();
 
-          OpportunitiesService.GET()
-              .then(opportunities => this.opportunities.data = opportunities)
-              .catch(() => PageErrorsHandlerService.notifyError())
-              .finally(() => (this.opportunities.isFetchLoading = false));
+        OpportunitiesService.GET()
+          .then(opportunities => this.opportunities.data = opportunities)
+          .catch(() => PageErrorsHandlerService.notifyError())
+          .finally(() => (this.opportunities.isFetchLoading = false));
 
-          this.addVolunteerOn = (opportunity) => {
-              const signedInUserId = JwtService.getToken().value;
-              const modifiedVolunteers = [...opportunity.volunteers, signedInUserId];
+        this.addVolunteerOn = (opportunity) => {
+          const signedInUserId = JwtService.getToken().value;
+          const modifiedVolunteers = [...opportunity.volunteers, signedInUserId];
               
-              OpportunitiesService.PATCH(opportunity.id, modifiedVolunteers)
-                .then(() => opportunity.volunteers = modifiedOpportunity) 
-          };
+          OpportunitiesService.PATCH(opportunity.id, modifiedVolunteers)
+            .then(() => opportunity.volunteers = modifiedVolunteers) 
+        };
+
+        this.toggleButton = (volunteers) => {
+         return volunteers.find(e => e === JwtService.getToken().value);
+        }
       }
     ]
   })
